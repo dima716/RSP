fc0e0dbc5f3ff3250b97ff6e87711ae7bfc93062
Anikeychik Dmitry <Dima716@yandex.ru>
2015-02-24 16:22:51 +0300

    Added listener for drag start event

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 RSP = function(){
-  var dndSupported; // true if drag and drop supported
+var dndSupported; // true if drag and drop supported
 
-  function detectDragAndDrop() {
-    if(navigator.appName == "Microsoft Internet Explorer") {
-      var ua = navigator.userAgent;
-      re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})=");
-      if(re.exec(ua) != null) {
-        var rv = parseFloat( RegExp.$1 );
-        if(rv >= 6.0) return true;
-      }
-      return false;
-
+function detectDragAndDrop() {
+  if(navigator.appName == "Microsoft Internet Explorer") {
+    var ua = navigator.userAgent;
+    re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})=");
+    if(re.exec(ua) != null) {
+      var rv = parseFloat( RegExp.$1 );
+      if(rv >= 6.0) return true;
     }
-    if ("draggable" in document.createElement("span")) return true;
     return false;
+
   }
+  if ("draggable" in document.createElement("span")) return true;
+  return false;
+}
+
+function handleDragStart(event) {
+  statusMessage("Drag started");
+}
+
+function setUpListeners() {
+  element("img1").addEventListener("dragstart", handleDragStart, false);
+}
+
 
 return {
   init: function() {
     if (dndSupported = detectDragAndDrop()) {
       statusMessage("Using HTML5 DragAndDrop");
+      setUpListeners();
     } else {
       statusMessage("This browser doesn't support Drag and Drop");
     }
